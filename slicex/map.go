3b9aa2db7414d088fa49cd87e6f6dadffd6e680c
SmartWalle <smartwalle@gmail.com>
2025-07-01 08:30:40 +0800

    添加 MapMatched

--- slicex/map.go
+++ slicex/map.go
@@ -2,13 +2,27 @@ package slicex
 
 // Map 对 slice 中的元素进行转换
 func Map[T any, N any](slice []T, fn func(elem T) N) []N {
-	n := len(slice)
+	var n = len(slice)
 	if n == 0 {
 		return nil
 	}
 	var ns = make([]N, 0, n)
 	for _, elem := range slice {
 		ns = Append(ns, fn(elem))
+	}
+	return ns
+}
+
+func MapMatched[T any, N any](slice []T, filterFunc func(elem T) bool, mapFunc func(elem T) N) []N {
+	var n = len(slice)
+	if n == 0 {
+		return nil
+	}
+	var ns = make([]N, 0, n)
+	for _, elem := range slice {
+		if filterFunc(elem) {
+			ns = Append(ns, mapFunc(elem))
+		}
 	}
 	return ns
 }
--- slicex/map_test.go
+++ slicex/map_test.go
@@ -27,3 +27,35 @@ func TestMap(t *testing.T) {
 		}
 	}
 }
+
+func TestMapMatched(t *testing.T) {
+	var tests = []struct {
+		source     []int
+		expect     []string
+		filterFunc func(elem int) bool
+		mapFunc    func(elem int) string
+	}{
+		{source: []int{1, 2, 3, 4, 5, 6}, expect: []string{"1", "2", "3", "4", "5", "6"}, filterFunc: func(elem int) bool {
+			return true
+		}, mapFunc: func(elem int) string {
+			return strconv.FormatInt(int64(elem), 10)
+		}},
+		{source: []int{1, 2, 3, 4, 5, 6}, expect: []string{"2", "4", "6", "8", "10", "12"}, filterFunc: func(elem int) bool {
+			return true
+		}, mapFunc: func(elem int) string {
+			return strconv.FormatInt(int64(elem*2), 10)
+		}},
+		{source: []int{1, 2, 3, 4, 5, 6}, expect: []string{"2", "4", "6"}, filterFunc: func(elem int) bool {
+			return elem%2 == 0
+		}, mapFunc: func(elem int) string {
+			return strconv.FormatInt(int64(elem), 10)
+		}},
+	}
+
+	for _, test := range tests {
+		var actual = slicex.MapMatched(test.source, test.filterFunc, test.mapFunc)
+		if !slicex.Equals(actual, test.expect, StringEqual) {
+			t.Fatalf("实际: %+v, 预期: %+v", actual, test.expect)
+		}
+	}
+}
