ee626dc35b6f170ed0c9cac16160ef8b477356b0
SmartWalle <smartwalle@gmail.com>
2025-09-20 10:36:49 +0800

    slicex.AggregateMatched

--- slicex/aggregate.go
+++ slicex/aggregate.go
@@ -1,10 +1,21 @@
 package slicex
 
-// Aggregate 对 slice 中的元素进行归约操作
-func Aggregate[T any, R any](slice []T, initial R, fn func(seed R, elem T) R) R {
-	var seed = initial
+// Aggregate 对 slice 中的元素进行聚合操作
+func Aggregate[T any, R any](slice []T, initial R, fn func(acc R, elem T) R) R {
+	var accumulator = initial
 	for _, elem := range slice {
-		seed = fn(seed, elem)
+		accumulator = fn(accumulator, elem)
 	}
-	return seed
+	return accumulator
+}
+
+// AggregateMatched 对 slice 中满足条件的元素进行聚合操作
+func AggregateMatched[T any, R any](slices []T, initial R, predicate func(elem T) bool, fn func(acc R, elem T) R) R {
+	var accumulator = initial
+	for _, elem := range slices {
+		if predicate(elem) {
+			accumulator = fn(accumulator, elem)
+		}
+	}
+	return accumulator
 }
