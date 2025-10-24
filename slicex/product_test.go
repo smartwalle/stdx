c2a249299811dbd8fd271eabd96b1839b160131f
SmartWalle <smartwalle@gmail.com>
2025-10-24 15:19:08 +0800

    CartesianProduct

---
+++ slicex/product_test.go
@@ -0,0 +1,16 @@
+package slicex_test
+
+import (
+	"github.com/smartwalle/stdx/slicex"
+	"testing"
+)
+
+func TestCartesianProduct(t *testing.T) {
+	var l1 = []string{"1", "2", "3", "4"}
+	var l2 = []string{"A", "B", "C", "D"}
+	var l3 = []string{"★", "☆"}
+
+	var p = [][]string{l1, l2, l3}
+
+	t.Log(slicex.CartesianProduct(p))
+}
