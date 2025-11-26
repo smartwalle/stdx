b34398aa1a39347710c9e48e8fd6c607162e4001
SmartWalle <smartwalle@gmail.com>
2025-11-27 07:50:19 +0800

    Group 函数调整

--- slicex/group.go
+++ slicex/group.go
@@ -1,22 +1,22 @@
 package slicex
 
 // Group 对 slice 中的元素进行分组操作
-func Group[T any, K comparable](slice []T, fn func(elem T) K) map[K][]T {
-	var newMap = make(map[K][]T)
+func Group[T any, K comparable, R any](slice []T, fn func(elem T) (K, R)) map[K][]R {
+	var newMap = make(map[K][]R)
 	for _, elem := range slice {
-		var key = fn(elem)
-		newMap[key] = append(newMap[key], elem)
+		var key, value = fn(elem)
+		newMap[key] = append(newMap[key], value)
 	}
 	return newMap
 }
 
 // GroupMatched 对 slice 中满足条件的元素进行分组操作
-func GroupMatched[T any, K comparable](slice []T, predicate func(elem T) bool, fn func(elem T) K) map[K][]T {
-	var newMap = make(map[K][]T)
+func GroupMatched[T any, K comparable, R any](slice []T, predicate func(elem T) bool, fn func(elem T) (K, R)) map[K][]R {
+	var newMap = make(map[K][]R)
 	for _, elem := range slice {
 		if predicate(elem) {
-			var key = fn(elem)
-			newMap[key] = append(newMap[key], elem)
+			var key, value = fn(elem)
+			newMap[key] = append(newMap[key], value)
 		}
 	}
 	return newMap
--- slicex/group_test.go
+++ slicex/group_test.go
@@ -7,8 +7,8 @@ import (
 
 func TestGroup(t *testing.T) {
 	var values = []int{35, 34, 33, 32, 31, 1, 2, 3, 4, 5, 11, 12, 13, 14, 15, 21, 22, 23, 24, 25}
-	var actual = slicex.Group(values, func(elem int) int {
-		return elem % 10
+	var actual = slicex.Group(values, func(elem int) (int, int) {
+		return elem % 10, elem
 	})
 	t.Log(actual)
 }
@@ -17,8 +17,8 @@ func TestGroupMatched(t *testing.T) {
 	var values = []int{35, 34, 33, 32, 31, 1, 2, 3, 4, 5, 11, 12, 13, 14, 15, 21, 22, 23, 24, 25}
 	var actual = slicex.GroupMatched(values, func(elem int) bool {
 		return elem%10 != 1
-	}, func(elem int) int {
-		return elem % 10
+	}, func(elem int) (int, int) {
+		return elem % 10, elem
 	})
 	t.Log(actual)
 }
