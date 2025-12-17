a49591ee28359787e0deffe8a63d41979b25e76e
SmartWalle <smartwalle@gmail.com>
2025-12-17 08:46:32 +0800

    Filter

--- slicex/filter.go
+++ slicex/filter.go
@@ -28,3 +28,36 @@ func OptimizedFilter[T any](slice []T, fn func(elem T) bool) []T {
 	}
 	return ns
 }
+
+func FilterOne[T any](slice []T, fn func(a, b T) T) (r T) {
+	if len(slice) == 0 {
+		return r
+	}
+	r = slice[0]
+	for _, elem := range slice[1:] {
+		r = fn(r, elem)
+	}
+	return r
+}
+
+// Min 返回切片中最小的元素，如果切片为空则返回零值,
+// fn 函数用于比较两个元素，返回 true 表示第一个元素小于第二个元素
+func Min[T any](slice []T, fn func(a, b T) bool) (r T) {
+	return FilterOne(slice, func(a, b T) T {
+		if fn(a, b) {
+			return a
+		}
+		return b
+	})
+}
+
+// Max 返回切片中最大的元素，如果切片为空则返回零值,
+// fn 函数用于比较两个元素，返回 true 表示第一个元素大于第二个元素
+func Max[T any](slice []T, fn func(a, b T) bool) (r T) {
+	return FilterOne(slice, func(a, b T) T {
+		if fn(a, b) {
+			return a
+		}
+		return b
+	})
+}
