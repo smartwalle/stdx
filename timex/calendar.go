c7f978c86893117f0586feb8b486c355ae1a2202
SmartWalle <smartwalle@gmail.com>
2026-07-04 11:56:45 +0800

    feat(timex): 新增日期/周/月/季度/年的范围与时间点便捷方法

--- timex/calendar.go
+++ timex/calendar.go
@@ -30,6 +30,28 @@ func (c Calendar) EndOfDay(year int, month time.Month, day int) time.Time {
 	return time.Date(year, month, day, 23, 59, 59, int(time.Second-time.Nanosecond), c.Location())
 }
 
+// DayRange 获取当前日历时区指定日期的开始时间和结束时间
+func (c Calendar) DayRange(year int, month time.Month, day int) (start, end time.Time) {
+	return c.BeginningOfDay(year, month, day), c.EndOfDay(year, month, day)
+}
+
+// BeginningOfDayAt 获取指定时间在当前日历时区所在日期的开始时间
+func (c Calendar) BeginningOfDayAt(t time.Time) time.Time {
+	t = t.In(c.Location())
+	return c.BeginningOfDay(t.Date())
+}
+
+// EndOfDayAt 获取指定时间在当前日历时区所在日期的结束时间
+func (c Calendar) EndOfDayAt(t time.Time) time.Time {
+	t = t.In(c.Location())
+	return c.EndOfDay(t.Date())
+}
+
+// DayRangeAt 获取指定时间在当前日历时区所在日期的开始时间和结束时间
+func (c Calendar) DayRangeAt(t time.Time) (start, end time.Time) {
+	return c.BeginningOfDayAt(t), c.EndOfDayAt(t)
+}
+
 // BeginningOfWeek 获取当前日历时区指定日期所在周的开始时间
 func (c Calendar) BeginningOfWeek(year int, month time.Month, day int) time.Time {
 	var t = time.Date(year, month, day, 0, 0, 0, 0, c.Location())
@@ -46,6 +68,28 @@ func (c Calendar) EndOfWeek(year int, month time.Month, day int) time.Time {
 	return time.Date(year, month, day+d, 23, 59, 59, int(time.Second-time.Nanosecond), c.Location())
 }
 
+// WeekRange 获取当前日历时区指定日期所在周的开始时间和结束时间
+func (c Calendar) WeekRange(year int, month time.Month, day int) (start, end time.Time) {
+	return c.BeginningOfWeek(year, month, day), c.EndOfWeek(year, month, day)
+}
+
+// BeginningOfWeekAt 获取指定时间在当前日历时区所在周的开始时间
+func (c Calendar) BeginningOfWeekAt(t time.Time) time.Time {
+	t = t.In(c.Location())
+	return c.BeginningOfWeek(t.Date())
+}
+
+// EndOfWeekAt 获取指定时间在当前日历时区所在周的结束时间
+func (c Calendar) EndOfWeekAt(t time.Time) time.Time {
+	t = t.In(c.Location())
+	return c.EndOfWeek(t.Date())
+}
+
+// WeekRangeAt 获取指定时间在当前日历时区所在周的开始时间和结束时间
+func (c Calendar) WeekRangeAt(t time.Time) (start, end time.Time) {
+	return c.BeginningOfWeekAt(t), c.EndOfWeekAt(t)
+}
+
 // BeginningOfMonth 获取当前日历时区指定月份的开始时间
 func (c Calendar) BeginningOfMonth(year int, month time.Month) time.Time {
 	return time.Date(year, month, 1, 0, 0, 0, 0, c.Location())
@@ -54,6 +98,28 @@ func (c Calendar) BeginningOfMonth(year int, month time.Month) time.Time {
 // EndOfMonth 获取当前日历时区指定月份的结束时间
 func (c Calendar) EndOfMonth(year int, month time.Month) time.Time {
 	return time.Date(year, month+1, 0, 23, 59, 59, int(time.Second-time.Nanosecond), c.Location())
+}
+
+// MonthRange 获取当前日历时区指定月份的开始时间和结束时间
+func (c Calendar) MonthRange(year int, month time.Month) (start, end time.Time) {
+	return c.BeginningOfMonth(year, month), c.EndOfMonth(year, month)
+}
+
+// BeginningOfMonthAt 获取指定时间在当前日历时区所在月份的开始时间
+func (c Calendar) BeginningOfMonthAt(t time.Time) time.Time {
+	t = t.In(c.Location())
+	return c.BeginningOfMonth(t.Year(), t.Month())
+}
+
+// EndOfMonthAt 获取指定时间在当前日历时区所在月份的结束时间
+func (c Calendar) EndOfMonthAt(t time.Time) time.Time {
+	t = t.In(c.Location())
+	return c.EndOfMonth(t.Year(), t.Month())
+}
+
+// MonthRangeAt 获取指定时间在当前日历时区所在月份的开始时间和结束时间
+func (c Calendar) MonthRangeAt(t time.Time) (start, end time.Time) {
+	return c.BeginningOfMonthAt(t), c.EndOfMonthAt(t)
 }
 
 // BeginningOfQuarter 获取当前日历时区指定季度的开始时间
@@ -68,6 +134,28 @@ func (c Calendar) EndOfQuarter(year int, quarter int) time.Time {
 	return time.Date(year, m+1, 0, 23, 59, 59, int(time.Second-time.Nanosecond), c.Location())
 }
 
+// QuarterRange 获取当前日历时区指定季度的开始时间和结束时间
+func (c Calendar) QuarterRange(year int, quarter int) (start, end time.Time) {
+	return c.BeginningOfQuarter(year, quarter), c.EndOfQuarter(year, quarter)
+}
+
+// BeginningOfQuarterAt 获取指定时间在当前日历时区所在季度的开始时间
+func (c Calendar) BeginningOfQuarterAt(t time.Time) time.Time {
+	t = t.In(c.Location())
+	return c.BeginningOfQuarter(t.Year(), Quarter(t.Month()))
+}
+
+// EndOfQuarterAt 获取指定时间在当前日历时区所在季度的结束时间
+func (c Calendar) EndOfQuarterAt(t time.Time) time.Time {
+	t = t.In(c.Location())
+	return c.EndOfQuarter(t.Year(), Quarter(t.Month()))
+}
+
+// QuarterRangeAt 获取指定时间在当前日历时区所在季度的开始时间和结束时间
+func (c Calendar) QuarterRangeAt(t time.Time) (start, end time.Time) {
+	return c.BeginningOfQuarterAt(t), c.EndOfQuarterAt(t)
+}
+
 // BeginningOfYear 获取当前日历时区指定年份的开始时间
 func (c Calendar) BeginningOfYear(year int) time.Time {
 	return time.Date(year, time.January, 1, 0, 0, 0, 0, c.Location())
@@ -76,6 +164,33 @@ func (c Calendar) BeginningOfYear(year int) time.Time {
 // EndOfYear 获取当前日历时区指定年份的结束时间
 func (c Calendar) EndOfYear(year int) time.Time {
 	return time.Date(year, time.December+1, 0, 23, 59, 59, int(time.Second-time.Nanosecond), c.Location())
+}
+
+// YearRange 获取当前日历时区指定年份的开始时间和结束时间
+func (c Calendar) YearRange(year int) (start, end time.Time) {
+	return c.BeginningOfYear(year), c.EndOfYear(year)
+}
+
+// BeginningOfYearAt 获取指定时间在当前日历时区所在年份的开始时间
+func (c Calendar) BeginningOfYearAt(t time.Time) time.Time {
+	t = t.In(c.Location())
+	return c.BeginningOfYear(t.Year())
+}
+
+// EndOfYearAt 获取指定时间在当前日历时区所在年份的结束时间
+func (c Calendar) EndOfYearAt(t time.Time) time.Time {
+	t = t.In(c.Location())
+	return c.EndOfYear(t.Year())
+}
+
+// YearRangeAt 获取指定时间在当前日历时区所在年份的开始时间和结束时间
+func (c Calendar) YearRangeAt(t time.Time) (start, end time.Time) {
+	return c.BeginningOfYearAt(t), c.EndOfYearAt(t)
+}
+
+// Quarter 获取指定月份所在季度
+func Quarter(month time.Month) int {
+	return int(month-1)/3 + 1
 }
 
 // BeginningOfDay 获取本地时区指定日期的开始时间
